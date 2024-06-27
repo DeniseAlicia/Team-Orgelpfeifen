57a9639db507c5e03082e718882c6dd25e7de11c
Denise <denisealiciafranz@gmail.com>
2024-06-27 12:34:53 +0200

    Update

--- Connect4/Connect4.js
+++ Connect4/Connect4.js
@@ -6,18 +6,34 @@ let columns = 7;
 let rows = 6;
 function handleLoad(_event) {
     console.log("page has loaded");
+    let startButton = document.getElementById("buttoncreateboard");
+    startButton.addEventListener("click", handleStart);
 }
-function createBoard(rows, columns) {
+function handleStart(_event) {
+    columns = parseInt(document.getElementById("columns").value);
+    rows = parseInt(document.getElementById("rows").value);
+    createBoard(rows, columns);
+}
+function createBoard(x, y) {
     console.log("board is being created...");
-    for (let i = 0; i < columns; i++) {
-        for (let t = 0; t < rows; t++) {
+    let board = document.getElementById("board");
+    for (let i = 0; i < y; i++) {
+        let rowHeight = i * 10;
+        for (let t = 0; t < x; t++) {
             let box = document.createElement("div");
+            box.className = "box";
+            box.style.display = "inline";
             box.style.position = "absolute";
+            box.style.top = rowHeight + "px";
+            box.id = String(x) + String(y);
+            if (x = t)
+                board.appendChild(box);
         }
     }
 }
 function placeCoin(_event) {
     console.log("coin placement");
+    let coinSpot = _event.target;
 }
 function checkWin() {
     console.log("checking for victory");
--- Connect4/Connect4.ts
+++ Connect4/Connect4.ts
@@ -9,27 +9,53 @@ let rows: number = 6;
 
 function handleLoad(_event: Event): void {
     console.log("page has loaded");
+    let startButton: HTMLElement = <HTMLElement>document.getElementById("buttoncreateboard");
+    startButton.addEventListener("click", handleStart);
 
 
 
-    
 
 }
 
-function createBoard(rows: number, columns: number): void {
-    console.log("board is being created...");
-    for(let i:number = 0; i < columns; i++){
+function handleStart(_event: MouseEvent): void {
 
-        for (let t:number = 0; t < rows; t++){
+    columns = parseInt((<HTMLInputElement>document.getElementById("columns")).value);
+    rows = parseInt((<HTMLInputElement>document.getElementById("rows")).value);
+
+    createBoard(rows, columns);
+}
+
+function createBoard(x: number, y: number): void {
+    console.log("board is being created...");
+    let board: HTMLElement = <HTMLElement>document.getElementById("board");
+
+    for (let i: number = 0; i < y; i++) {
+
+        let rowHeight: number = i * 10
+
+        for (let t: number = 0; t < x; t++) {
 
             let box: HTMLElement = document.createElement("div");
-            box.style.position = "absolute"
+            box.className = "box";
+            box.style.display = "inline";
+            box.style.position = "absolute";
+            box.style.top = rowHeight + "px";
+            box.id = String(x) + String(y);
+            
+            if (x = t)
+
+            board.appendChild(box);
         }
     }
+
+
 }
 
 function placeCoin(_event: MouseEvent): void {
     console.log("coin placement");
+
+    let coinSpot: HTMLElement = <HTMLElement>_event.target;
+
 }
 
 function checkWin(): void {
